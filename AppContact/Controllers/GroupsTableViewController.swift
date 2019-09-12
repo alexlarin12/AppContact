c63070a3a26a407e0c9bcbbce94793b3d1bec657
alexlarin <alex12081973@mail.ru>
2019-09-12 21:13:36 +0300

    lesson9

--- AppContact/Controllers/GroupsTableViewController.swift
+++ AppContact/Controllers/GroupsTableViewController.swift
@@ -101,18 +101,8 @@ class GroupsTableViewController: UITableViewController {
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "WatchGroup",
-            let groupController = segue.destination as? GroupViewController,
-            let indexPath = tableView.indexPathForSelectedRow {
-            let name = groups[indexPath.row].groupName
-            let image = groups[indexPath.row].groupImage
-            groupController.groupNameForTitle = name
-        
-        }
-        
-        
-        
-        
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
     
 
